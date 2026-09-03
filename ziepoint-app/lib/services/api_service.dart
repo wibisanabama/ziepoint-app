b83b722298591642fe436995da03b441560ec10f
wibisanabama <wibisanabama@gmail.com>
2026-09-04 06:12:29 +0700

    fix: use Android emulator backend address

--- ziepoint-app/lib/services/api_service.dart
+++ ziepoint-app/lib/services/api_service.dart
@@ -5,7 +5,7 @@ import '../models/guru.dart';
 import '../models/jenis_catatan.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://10.0.2.2:3000';
 
   Future<List<Siswa>> fetchSiswa() async {
     final response = await http.get(Uri.parse('$baseUrl/siswa'));
